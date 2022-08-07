6755db844221fc9155aad2179b1efb6f73d6bbb2
zaidi <zaidi.asyraf@yahoo.com>
2022-08-07 22:28:51 +0800

    first commit

---
+++ src/main/java/com/zaidi/springrabbitmq/SpringRabbitMqApplication.java
@@ -0,0 +1,13 @@
+package com.zaidi.springrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRabbitMqApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRabbitMqApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zaidi/springrabbitmq/SpringRabbitMqApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zaidi.springrabbitmq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRabbitMqApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
